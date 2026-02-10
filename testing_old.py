4d42b21d62e161f870eb3c19e2f658b107a1e8c8
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-02-10 15:54:01 +0100

    some light data handling

--- testing_old.py
+++ testing_old.py
@@ -6,10 +6,12 @@ adata = sc.read_h5ad("df2.h5ad")
 
 #print(adata)
 
-print(adata.obs.head())
+#print(adata.obs.head())
 #print(adata.var.head())
 
-#sc.pp.calculate_qc_metrics(adata, inplace=True)
-#sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True)
+mets = sc.pp.calculate_qc_metrics(adata)
+print(mets)
+
+#sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts'], multi_panel=True)
 
 #print(adata.var_names)
