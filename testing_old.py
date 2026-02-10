075712e1d6a2b3e873110747b194cd4dd66c8063
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-02-10 15:37:41 +0100

    some datahandling + added anndata explainer

--- testing_old.py
+++ testing_old.py
@@ -1,5 +1,15 @@
 import scanpy as sc
 #import anndata as ad
 
-print(sc.__file__)
-#adata = sc.read_h5ad("df2.h5ad")
+#print(sc.__version__)
+adata = sc.read_h5ad("df2.h5ad")
+
+#print(adata)
+
+print(adata.obs.head())
+#print(adata.var.head())
+
+#sc.pp.calculate_qc_metrics(adata, inplace=True)
+#sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'], jitter=0.4, multi_panel=True)
+
+#print(adata.var_names)
