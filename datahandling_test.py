ab0c5f44639724935543bdf21d55f0aa3cd0390c
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-02-16 14:55:18 +0100

    formatting

--- datahandling_test.py
+++ datahandling_test.py
@@ -1,9 +1,5 @@
 import scanpy as sc
-#import anndata as ad
-
 #print(sc.__version__)
-#print(adata.obs.head())
-#print(adata.var.head())
 
 def check_ok(f):
     # ensure that the file was properly saved
@@ -16,18 +12,18 @@ adata = check_ok("Vasculature_cells.h5ad")
 print(adata)
 
 # See cell clusters and batch info
-print("\n cell clusters and batch info:")
+print("\n + cell clusters and batch info:")
 print(adata.obs.head())
 
 # List all available metadata columns
-print("\n metadata cols:")
+print("\n + metadata cols:")
 print(adata.obs_names)
 
 # This prints all available category names you can use for plotting
 print(adata.obs.columns.tolist())
 
 # Check the type and shape
-print("\n type and shape:")
+print("\n + type and shape:")
 print(type(adata.X))
 print(adata.X.shape)
 
@@ -37,21 +33,20 @@ print("\n + slice of raw counts:")
 print(adata.X[:10, :10].toarray())
 
 # See gene names and any HVG (Highly Variable Gene) info
-print("\n gene names, HVG info:")
+print("\n + gene names, HVG info:")
 print(adata.var.head())
 
 # List the names of all available reductions
-print("\n av. reductions:")
+print("\n + av. reductions:")
 print(adata.obsm.keys())
 
 # View the actual UMAP coordinates
-print("\n umap coords:")
+print("\n + umap coords:")
 print(adata.obsm['X_umap'][:5])
 
 # This prints all available category names you can use for plotting
 print(adata.obs.columns.tolist())
 
 # Plot UMAP colored by clusters
-print("\n plotting umap:")
+print("\n + plotting umap:")
 sc.pl.umap(adata, color='ident', frameon=False, title="Imported Seurat Clusters")
-
