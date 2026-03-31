8d2d5ac69d097f95d12bf44f39fbdce4df126b6d
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-03-31 13:49:54 +0200

    restructure, imports changed

--- BINN/data_handling.py
+++ BINN/data_handling.py
@@ -7,31 +7,18 @@ import numpy as np
 import scipy.sparse as sp
 from preprocessing import custom_train_test_split
 import anndata as ad
-
-class SingleCellDataset(ad.AnnData):
-    """
-    Custom Pytorch dataset for our anndata objects, for feeding into loaders
-    """
-    def __init__(self, X, y):
-        self.X = X
-        self.y = torch.LongTensor(y)
-
-    def __len__(self):
-        return self.X.shape[0]
-
-    def __getitem__(self, idx):
-        # Convert sparse row to dense tensor on the fly to save memory
-        row = self.X[idx]
-        if sp.issparse(row):
-            row = row.toarray().squeeze()
-        return torch.FloatTensor(row), self.y[idx]
+import SingleCellDataset
     
-## READING DATA, SPLITTING
 def read_adata(indices: list, train_size=0.8):
+    """
+    """
     train_adata, test_adata, acollection = custom_train_test_split.pipeline(indices, train_size)
     return train_adata, test_adata, acollection
     
 def train_test_adatasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
+    """
+    Creates the train/test split for the input anndata
+    """
     X_train = train_adata.X
     y_train = train_adata.obs["AD_status"]
     X_test = test_adata.X
@@ -41,7 +28,7 @@ def train_test_adatasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
 
 ALL_CELLTYPES = [0,1,2,3,4,5,6,7,8]
 
-def get_dataloaders(adata, label_col='cell_type', batch_size=64, test_size=0.2):
+def get_dataloaders(adata : ad.AnnData, label_col='cell_type', batch_size=64, test_size=0.2):
     """
     Extracts data from AnnData and returns train and test DataLoaders.
     """
@@ -60,3 +47,4 @@ def get_dataloaders(adata, label_col='cell_type', batch_size=64, test_size=0.2):
 
     return train_loader, test_loader
 
+read_adata([0])
