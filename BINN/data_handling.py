df8eb8b1a0c21bfbf03c104cae5006fa2abc574d
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-03-31 13:39:05 +0200

    restructure

--- BINN/data_handling.py
+++ BINN/data_handling.py
@@ -1,3 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import scipy.sparse as sp
 from preprocessing import custom_train_test_split
 import anndata as ad
 
@@ -13,3 +20,23 @@ def train_test_adatasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
 
     return X_train, y_train, X_test, y_test
 
+ALL_CELLTYPES = [0,1,2,3,4,5,6,7,8]
+
+class SingleCellDataset(ad.AnnData):
+    """
+    Custom Pytorch dataset for our anndata objects, for feeding into loaders
+    """
+    def __init__(self, X, y):
+        self.X = X
+        self.y = torch.LongTensor(y)
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        # Convert sparse row to dense tensor on the fly to save memory
+        row = self.X[idx]
+        if sp.issparse(row):
+            row = row.toarray().squeeze()
+        return torch.FloatTensor(row), self.y[idx]
+    
