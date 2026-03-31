504e1fe3d690dbc19657a1cd0ab9fea46a8c3ab9
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-03-31 13:45:10 +0200

    updated data_handling

--- BINN/data_handling.py
+++ BINN/data_handling.py
@@ -8,20 +8,6 @@ import scipy.sparse as sp
 from preprocessing import custom_train_test_split
 import anndata as ad
 
-def read_adata(indices: list, train_size=0.8):
-    train_adata, test_adata, acollection = custom_train_test_split.pipeline(indices, train_size)
-    return train_adata, test_adata, acollection
-    
-def train_test_adatasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
-    X_train = train_adata.X
-    y_train = train_adata.obs["AD_status"]
-    X_test = test_adata.X
-    y_test = test_adata.obs["AD_status"]
-
-    return X_train, y_train, X_test, y_test
-
-ALL_CELLTYPES = [0,1,2,3,4,5,6,7,8]
-
 class SingleCellDataset(ad.AnnData):
     """
     Custom Pytorch dataset for our anndata objects, for feeding into loaders
@@ -40,3 +26,37 @@ class SingleCellDataset(ad.AnnData):
             row = row.toarray().squeeze()
         return torch.FloatTensor(row), self.y[idx]
     
+## READING DATA, SPLITTING
+def read_adata(indices: list, train_size=0.8):
+    train_adata, test_adata, acollection = custom_train_test_split.pipeline(indices, train_size)
+    return train_adata, test_adata, acollection
+    
+def train_test_adatasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
+    X_train = train_adata.X
+    y_train = train_adata.obs["AD_status"]
+    X_test = test_adata.X
+    y_test = test_adata.obs["AD_status"]
+
+    return X_train, y_train, X_test, y_test
+
+ALL_CELLTYPES = [0,1,2,3,4,5,6,7,8]
+
+def get_dataloaders(adata, label_col='cell_type', batch_size=64, test_size=0.2):
+    """
+    Extracts data from AnnData and returns train and test DataLoaders.
+    """
+    X = adata.X
+    y = adata.obs[label_col].values
+
+    X_train, X_test, y_train, y_test = train_test_adatasplit(
+        X, y, test_size=test_size, stratify=y, random_state=42
+    )
+
+    train_dataset = SingleCellDataset(X_train, y_train)
+    test_dataset = SingleCellDataset(X_test, y_test)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
