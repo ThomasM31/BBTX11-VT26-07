04da96ca12d40da1c3ffa6336470ceefef8951e7
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-03-31 16:12:59 +0200

    some more pipeline

--- BINN/binn_pipeline.py
+++ BINN/binn_pipeline.py
@@ -7,9 +7,32 @@
 # Evaluate + Interpret BINN
 # Compare to SVM
 
+import argparse
+import os
+
 def pipeline(
         to_include: list, 
         train_size: int,
-        
+
     ) -> None:
-        pass
+        
+        
+
+        print('Pipeline completed')
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "Run annData objects through BINN pipeline"
+    )
+
+    # Positional argument: accepts one or more integers
+    parser.add_argument(
+        "to_include",
+        type=int,        
+        nargs='+',       
+        help="indices to include: \n0=astro \n1=exc1 \n2=exc2 \n3=exc3 \n4=immune \n5=inhi \n6=oligo \n7=opcs \n8=vasc",
+    )
+
+
