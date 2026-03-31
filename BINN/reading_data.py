1ef7c3d7df44f4d8b2d31333d1f0115e412786c4
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-03-31 13:13:48 +0200

    updated function calls

--- BINN/reading_data.py
+++ BINN/reading_data.py
@@ -1,23 +1,8 @@
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-# BINN 
-from BINN.model import BINN
-from BINN.trainer import train_binn, test_binn
-
-from preprocessing import custom_train_test_split_modified
+from preprocessing import custom_train_test_split
 import anndata as ad
 
-in_features=99999 # Integers of how many genes we have as input
-layer_list=[] # List of integers, which describes how many layers and how many nodes each layer has
-mask_list=[] # List of binary tensors (binary matrices) that restricts each layer
-
-binn=BINN(in_features, layer_list, mask_list)
-
 def read_adata(indices: list, train_size=0.8):
-    train_adata, test_adata, collection = custom_train_test_split_modified.pipeline(indices, train_size)
+    train_adata, test_adata, collection = custom_train_test_split.pipeline(indices, train_size)
     return train_adata, test_adata, collection
     
 def xy_datasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
@@ -27,3 +12,4 @@ def xy_datasplit(train_adata: ad.AnnData, test_adata: ad.AnnData):
     y_test = test_adata.obs["AD_status"]
 
     return X_train, y_train, X_test, y_test
+
