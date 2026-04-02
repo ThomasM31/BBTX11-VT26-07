0e8a31247598216a121de9c798bc05ac07436b23
ThomasM31 <Thomasmathiassen99@gmail.com>
2026-04-02 14:31:25 +0200

    work on fetching relevant data + pipeline()

--- BINN/data_handling.py
+++ BINN/data_handling.py
@@ -34,9 +34,28 @@ def train_test_adatasplit(train_adata: ad.AnnData,
     return X_train, y_train, X_test, y_test
 
 def process_completed_data(datasets: dict) -> dict:
-    
+    datasets_proc = {}
+    for label, dataset in datasets.items():
+        print(f"fetching pseudo from {label}")
+        # fetch pseudo-batched & preprocessed data
+        datasets_proc[label] = dataset[label].uns['pseudo'].copy()
     return datasets_proc
 
+# From test run
+""" datasets:
+{'astro': AnnData object with n_obs × n_vars = 146638 × 18071
+    obs: 'cell_type_high_resolution', 'subject', 'ident', 'n_genes', 'n_genes_by_counts', 'total_counts', 'total_counts_mt', 'pct_counts_mt'
+    var: 'mt', 'n_cells_by_counts', 'mean_counts', 'pct_dropout_by_counts', 'total_counts'
+    uns: 'common_hvgs', 'pseudo'
+    obsm: 'X_umap'
+    layers: 'logcounts'}
+
+    pseudo uns for astro: AnnData object with n_obs × n_vars = 1234 × 1000
+    obs: 'subject', 'cell_type_high_resolution', 'n_obs_aggregated', 'AD_status'
+    var: 'mt', 'n_cells_by_counts', 'mean_counts', 'pct_dropout_by_counts', 'total_counts', 'mean', 'std'
+    uns: 'log1p'
+    layers: 'sum'
+    """
 
 def create_dataloaders(train_adata: ad.AnnData,
                        test_adata: ad.AnnData,
@@ -60,11 +79,20 @@ def save_data(datasets: dict, filepath: str) -> None:
 base_path = "/data/shared/alzgene26/data"
 data_path = base_path + "/processed_data/completed/mg_200_mc_200_mhvg1000/"
 save_path = "/data/users/thomath/kand/data/processed_data/extracted_from_completed/"
+ALL_CELLTYPES = [0,1,2,3,4,5,6,7,8]
 
 def pipeline() -> None:
-    datasets = ctts.read_files(to_include=[0,1,2,3,4,5,6,7,8], filepath=data_path)
+    print("Reading data into datasets...")
+    datasets = ctts.read_files(to_include=ALL_CELLTYPES, filepath=data_path)
+
+    print("Processing datasets...")
     datasets_proc = process_completed_data(datasets)
+
+    print("Saving data...")
     save_data(datasets_proc, save_path)
+    
+    # test
+    print(datasets_proc)
 
 
 if __name__ == "__main__":
