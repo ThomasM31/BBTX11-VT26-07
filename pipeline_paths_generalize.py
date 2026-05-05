c2146983f02eec8a0fa9725c2f6e24aa2aa394a8
bllnr <52850477+bllnr@users.noreply.github.com>
2026-05-05 15:22:14 +0200

    Generalizability data: Add preprocess pipeline and pipeline paths

---
+++ pipeline_paths_generalize.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import os
+
+class PipelinePaths:
+
+    def _ensure_paths_exist(self):
+        for attr_name in dir(self):
+            attr = getattr(self, attr_name)
+            if isinstance(attr, Path):
+                attr.mkdir(parents=True, exist_ok=True)
+
+    def __init__(self, 
+                 shared_dir_mode: bool, 
+                 full_pipeline_run_vars: str = '',
+                 pseudo_non_hvg_run_vars: str = '',
+                 hvg_non_pseudo_run_vars: str = ''
+                 ) -> None:
+
+        if shared_dir_mode:
+            # To read from and write to the shared folder
+            base_path = Path("/data/shared/alzgene26") 
+            data_path = base_path / "test_data"
+        else:
+            ### To read from and write to current users folders
+            user = str(os.environ.get('USER')) or str(os.environ.get('USERNAME'))
+            data_path = Path("/data/users") / user / "kand/data/"
+
+        processed_data = "processed_data"
+
+        # base experimental data before preprocessing
+        self.conv_data_path       = data_path / "conv_data"
+        # AD status, age etc
+        self.metadata_path        = data_path / "supplementary_data"
+
+        # intermediate data to save and read in pipeline
+        self.gene_expr_count_path = data_path / processed_data / "expr_counts"
+        self.genes_keep_path      = data_path / processed_data / "filter_genes"
+        self.hvg_lists_path       = data_path / processed_data / "hvg_lists"
+        self.hvg_common_path      = data_path / processed_data / "hvg_common"
+        
+        # to save umaps
+        self.figures_path         = data_path / "figures"
+        self.full_pipeline_figs   = self.figures_path / "full_pipeline" / full_pipeline_run_vars
+        self.danish_figs          = self.figures_path / "danish" 
+        self.pseudo_non_hvg_figs   = self.figures_path / "pseudo_non_hvg" / pseudo_non_hvg_run_vars
+        self.hvg_non_pseudo_figs  = self.figures_path / "hvg_non_pseudo" / hvg_non_pseudo_run_vars
+
+        self.figures_path_shap         = data_path / "figures" / "shap"
+        
+        # to save smaller files that are faster to read
+        self.test_data_path       = data_path / processed_data / "test_data"
+        
+        # reactome assets
+        self.pathway_data_path    = base_path / "PathwayData"
+        
+        # for saving fully preprocessed data
+        self.compl_base = data_path / processed_data / "completed"
+        
+        self.compl_full_pipe_path = self.compl_base / "full_pipeline" / full_pipeline_run_vars
+        self.compl_path_danish    = self.compl_base / "danish"
+        self.compl_hvg_non_pseudo = self.compl_base / "hvg_non_pseudo" / hvg_non_pseudo_run_vars
+        self.compl_pseudo_non_hvg = self.compl_base / "pseudo_non_hvg" / pseudo_non_hvg_run_vars
+
+        self.mask_base = self.pathway_data_path / "MaskMatrixLayers"
+        self.mask_full_pipe_path = self.mask_base / "full_pipeline" / full_pipeline_run_vars
+        self.mask_path_danish    = self.mask_base / "danish"
+        self.mask_hvg_non_pseudo = self.mask_base / "hvg_non_pseudo" / hvg_non_pseudo_run_vars
+        self.mask_pseudo_non_hvg = self.mask_base / "pseudo_non_hvg" / pseudo_non_hvg_run_vars
+
+        self.extra_test_path = data_path / processed_data/ "test_path"
+
+        # RESULTS
+        self.results_path = data_path / 'results'
+        self.binn_test_results_path = self.results_path / 'binn_results' 
+        self.svm_results_path = self.results_path / 'svm_results'
+        self.shap_results_path = self.results_path / 'shap_data'
+
+        # for every path, creates path if it does not exist
+        self._ensure_paths_exist()
---
+++ preprocessing/pipeline_preprocess_full_generalize.py
@@ -0,0 +1,158 @@
+import preprocess as pre
+import argparse
+import os
+from pathlib import Path
+import importlib.util
+import anndata as ad
+
+# import module for consistent paths
+path = Path(__file__).resolve().parent.parent / "pipeline_paths_generalize.py"
+spec = importlib.util.spec_from_file_location("ppaths", path)
+ppaths = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(ppaths)
+
+'''
+THE ACTUAL PREPROCESSING PIPELINE. USES DATA CREATED BY THE FIRST TWO PREP PIPELINES.
+RUN EACH DATASET SEPARATELY.
+'''
+
+def pipeline(
+        n_top_genes: int, 
+        min_genes: int, 
+        min_cells:int,
+        nr_common_hvgs:int,
+        common_hvg_inc_value:int,
+        shared_dir_mode: bool
+        ) -> None:
+    
+    run_vars = f'mg_{min_genes}_mc_{min_cells}_mhvg{nr_common_hvgs}' 
+    pp = ppaths.PipelinePaths(shared_dir_mode, full_pipeline_run_vars=run_vars)
+        
+    #-------START PROCESSING-------
+
+    included_labels = ['all']
+    
+    adata = ad.read_h5ad(pp.conv_data_path / 'GSE157827_merged.h5ad')
+    adata.obs = adata.obs.rename(columns={'group': 'subject'})
+    datasets = {'all': adata}
+
+    # get the order of the original genes (for verification after processing)
+    genes_ordered = datasets[included_labels[0]].var_names.tolist()
+
+    #-------PERFORM FILTERING-------
+    
+    # The cell filtering doesn't actually remove anything from our dataset using
+    # the default values, therefore commented out
+
+    # remove cells with less than 200 expressed genes
+    pre.filter_cells_by_min_genes(datasets, min_genes=200)
+    
+    # remove cells with mito gene expression over threshold
+    pre.filter_cells_by_mitochondrial_content(datasets)
+
+    # remove genes not in the final mask
+    astro_path = '/data/shared/alzgene26/data/processed_data/completed/full_pipeline/mg_200_mc_200_mhvg1000/astro.h5ad'
+    astro = ad.read_h5ad(astro_path)
+
+    astro_genes = astro.var_names.tolist()
+
+    adata = adata[:, astro_genes]
+    datasets['all'] = adata
+    print(adata)
+
+    #-------PSEUDOBULK AND NORMALIZE-------
+
+    # sum counts per subject and high res cell type
+    pre.pseudobulk(datasets, 'mean')
+    
+    #  normalize per pseudobulk sample
+    pre.normalize(datasets)
+
+    #-------MOVE PROCESSED DATA TO MAIN LAYER AND SAVE-------
+    
+    # move pseudobulk data to main layer, discard everything else
+    # this will make the files a lot smaller
+    pre.move_to_main(datasets, 'pseudo')
+
+    #-------ADD METADATA-------
+
+    # add disease status
+    adata.obs['AD_status'] = adata.obs['subject'].str.startswith('AD').astype(int)
+    datasets['all'] = adata
+
+    print(adata)
+
+    pre.save_files(datasets, pp.compl_full_pipe_path, 'completed')
+
+    print('Pipeline completed')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "Pre-process anndata objects and save as .h5ad"
+    )
+
+    # Optional argument n_top_genes
+    parser.add_argument(
+        "--n_top_genes", 
+        type=int,
+        default=2000,
+        help="Number of highly variable genes to keep (default: 2000)"
+    )
+
+    # Optional argument
+    parser.add_argument(
+        "--gene_in_min_cells", 
+        type=int,
+        default=200,
+        help="Filter out genes that are expressed in less than n cells."
+    )
+
+    # Optional argument
+    parser.add_argument(
+        "--cell_with_min_genes", 
+        type=int,
+        default=200,
+        help="Filter out cells that have less than n gene expressions."
+    )
+
+    # Optional argument
+    parser.add_argument(
+        "--nr_common_hvgs", 
+        type=int,
+        default=1000,
+        help="Target for minimum nr of common hvgs"
+    )
+
+    # Optional argument
+    parser.add_argument(
+        "--common_hvg_inc_value", 
+        type=int,
+        default=3000,
+        help="Amount to increment n_top_genes if nr common genes is under target."
+    )
+
+    # Optional argument
+    parser.add_argument(
+        "--shared_dir_mode", 
+        type=bool,
+        default=True,
+        help="Directory to work in: shared = True, user = False. Default is True."
+    )
+
+    args = parser.parse_args()
+
+    n_top = args.n_top_genes
+    min_cells = args.gene_in_min_cells
+    min_genes = args.cell_with_min_genes
+    nr_common_hvgs = args.nr_common_hvgs
+    common_hvg_inc_value = args.common_hvg_inc_value
+    shared_dir_mode = args.shared_dir_mode
+
+    pipeline(
+        n_top_genes=n_top, 
+        min_genes=min_genes, 
+        min_cells=min_cells, 
+        nr_common_hvgs=nr_common_hvgs, 
+        common_hvg_inc_value=common_hvg_inc_value,
+        shared_dir_mode = shared_dir_mode
+        )
