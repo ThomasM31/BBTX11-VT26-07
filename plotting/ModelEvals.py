c5b8e8008598782514a43d0edc065d69430406cb
bllnr <52850477+bllnr@users.noreply.github.com>
2026-05-11 11:46:55 +0200

    add svm read and generalizability read

--- plotting/ModelEvals.py
+++ plotting/ModelEvals.py
@@ -21,8 +21,16 @@ def load_binn_scores():
         return df['y_true'].values, df['y_prob'].values
     return None, None
 
+# Load generalizability results:
+def load_gen_scores(): 
+    p = "/data/shared/alzgene26/data/results/binn_results/gen_test_results_260508_0940_conv.csv"
+    if os.path.exists(p):
+        df = pd.read_csv(p) 
+        return df['y_true'].values, df['y_prob'].values
+    return None, None
+
 # Load SVM results:
-def load_svm_scores(cell_type):
+def load_svm_scores():
     if os.path.exists("plotting/ModelResults/svm_test_results.csv"):
         df = pd.read_csv("plotting/ModelResults/svm_test_results.csv")
         return df['y_true'].values, df['y_prob'].values
@@ -58,7 +66,7 @@ def draw_roc(ax, binn_data, svm_data):
     ax.set_ylabel("Sensitivitet")
     ax.legend(fontsize=8)
 
-def draw_confusion(ax, y_t, y_s):
+def draw_confusion(ax, y_t, y_s, label):
     # Plot the normalized confusion matrix for the BINN
     cm = confusion_matrix(y_t, (y_s >= 0.5).astype(int), normalize="true")
     im = ax.imshow(cm, cmap="Blues", vmin=0, vmax=1)
@@ -69,7 +77,7 @@ def draw_confusion(ax, y_t, y_s):
             color = "white" if cm[i,j] > 0.5 else "black"
             ax.text(j, i, f"{cm[i,j]:.2f}", ha="center", va="center", color=color)
             
-    ax.set_title("Normaliserad förväxlingsmatris: BINN")
+    ax.set_title("Normaliserad förväxlingsmatris: {label}")
     ax.set_xticks([0, 1], ["Frisk", "AD"])
     ax.set_yticks([0, 1], ["Frisk", "AD"])
     ax.set_xlabel("Förutspådd etikett")
@@ -95,5 +103,19 @@ def run_evaluation():
     else:
         print("Required data file 'binn_test_results.csv' not found. Please run the pipeline first.")
 
+def run_gen_evaluation():
+    # Orchestrate the loading and plotting process
+    y_t_binn, y_s_binn = load_binn_scores()    
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 5))
+    
+    draw_confusion(ax, y_t_binn, y_s_binn, 'BINN')
+    
+    plt.tight_layout()
+    save_path = os.path.join(SAVE_DIR, "binn_evaluation_swe.png")
+    plt.savefig(save_path, dpi=300)
+    print(f"Evaluation complete. Figure saved to: {save_path}")
+
+
 if __name__ == "__main__":
-    run_evaluation()
+    run_gen_evaluation()
